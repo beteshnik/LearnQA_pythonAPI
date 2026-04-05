6ff9f670a1ad6827e46a4761742a1b9a00aabc99
Tatyana Fedotova <beteshnik@mail.ru>
2026-04-05 19:30:19 +0300

    Задание недели 2 - Ex5: Парсинг JSON

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import requests
-response = requests.get("https://playground.learnqa.ru/api/get_text", verify=False)
-print(response.text)
+import json
+
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+json_obj = json.loads(json_text)
+print(json_obj["messages"][1]["message"])
