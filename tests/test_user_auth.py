8b8095891171ba356d0dbcf12c38a939553f0880
Tatyana Fedotova <beteshnik@mail.ru>
2026-04-11 20:39:55 +0300

    Create test_user_auth.py

---
+++ tests/test_user_auth.py
@@ -0,0 +1,45 @@
+import requests
+import pytest
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+class TestUserAuth(BaseCase):
+    exclude_params = [
+        ("no_cookie"),
+        ("no_token")
+    ]
+    def setup_method(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data, verify=False)
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+    def test_user_auth(self):
+        response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
+                                 headers={"x-csrf-token": self.token},
+                                 cookies={"auth_sid": self.auth_sid},
+                                 verify=False)
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
+            "Нет user_id в ответе 2"
+        )
+    @pytest.mark.parametrize("condition", exclude_params)
+    def test_negative_auth_check(self, condition):
+        if condition == "no_cookie":
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
+                                 headers={"x-csrf-token": self.token},
+                                 verify=False)
+        else:
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
+                                 cookies={"auth_sid": self.auth_sid},
+                                 verify=False)
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            "Нет user_id в ответе 2"
+        )
